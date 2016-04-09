3bb6572eb98d2228c2e5e3a2ad13b9fc0ac007ef
Jonas.J <jan.jonas.johansson@gmail.com>
2016-04-10 01:01:39 +0200

    you can now generate a link to share and follow that link to a specific rating-view

--- public/js/ctrl/voteingCtrl.js
+++ public/js/ctrl/voteingCtrl.js
@@ -1,6 +1,18 @@
-spotifyApp.controller('VoteingCtrl', function ($scope) {
-
+spotifyApp.controller('VoteingCtrl', function ($scope, fbService, $routeParams) {
+	
 	$scope.fillstar = "";
+	
+	$scope.getPlaylistData = function () {
+		// fetches specific playlist data from firebase.
+		fbService.getPlaylist($routeParams.playlistId).then(function (response) {
+			//sets all scope variables from response
+			//console.log(response)
+			$scope.owner = response.owner
+			$scope.title = response.name
+			$scope.spotLink = response.spotifyUrl
+		})
+	}
+
 	$scope.fillstars = function(star){
 		console.log(star);
 		if(star == 3){
@@ -13,6 +25,8 @@ spotifyApp.controller('VoteingCtrl', function ($scope) {
 		if(index === 3){
 			return 	{'fill':'#54BA5D'}	
 		}
-}
+	}
+
+
 
 });
--- public/js/service/firebaseService.js
+++ public/js/service/firebaseService.js
@@ -1,23 +1,34 @@
 spotifyApp.factory('fbService', function ($resource, $firebaseArray) {
 
-	//var playVoteRef = new Firebase("https://spotifyapplication.firebaseio.com/playVoteUrls");
-	// this.coursesRef = new Firebase("https://solvation.firebaseio.com/courses");
-	// var usersRef = new Firebase("https://solvation.firebaseio.com/userdata");
-
-	//var playVote = $firebaseArray(playVoteRef);
-	// var courses = $firebaseArray(this.coursesRef);
-	// var users = $firebaseArray(usersRef);
-
+	var playVoteRef = new Firebase("https://spotifyapplication.firebaseio.com/playVoteUrls");
+	var playVoteRef = $firebaseArray(playVoteRef);
+	
 	this.addPlayVoteUrl = function(data) {
 		var playVoteRef = new Firebase("https://spotifyapplication.firebaseio.com/playVoteUrls/" + data.id);
 	    playVoteRef.set( {
 	    	'playlistApiUrl': data.playlistApiUrl,
-	    	'rating':0, 'voteUrl': data.voteUrl,
-	    	'owner': data.owner
+	    	'spotifyUrl': data.spotifyUrl,
+	    	'rating':0, 
+	    	'voteUrl': data.voteUrl,
+	    	'owner': data.owner,
+	    	'name' : data.name
 	    	} );
+	    //the object in ref should be replaced totally with the parameter object data! When rating is initialized
 	}
 
+	this.getAllSharedPlaylists = function() {
+    	//returns all of the userds voteable playlists from firebase
+    	return playVoteRef.$loaded().then(function (response) {
+    		return response
+    	})
+	}
 
+	this.getPlaylist = function(playlistId) {
+    	//takes a playlisdId as input and returns specific playlist data.
+    	return playVoteRef.$loaded().then(function (response){
+    		return playVoteRef.$getRecord(playlistId)
+    	})
+	}
 
 	return this;
 });
