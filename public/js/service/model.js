16a2ce20a8cbc93d81edfcee45d19eaa25492f88
Jonas.J <jan.jonas.johansson@gmail.com>
2016-04-09 13:44:56 +0200

    added uniqe ID geneerator to prepare for unice voting views, then also added startup function for getting data to the profile view

--- public/js/service/model.js
+++ public/js/service/model.js
@@ -6,15 +6,6 @@ spotifyApp.factory('Model', function ($resource, $http, $q, $timeout) {
 	}
 
 	var self = this;
-	
-	this.signedIn = function () {
-		// body...
-		if (this.settings.access_token) {
-			return true
-		}else{
-			return false
-		}
-	}
 
 	var req = function (url) {
 		// returns a "spotify-ready" http request from the url argument
@@ -24,8 +15,27 @@ spotifyApp.factory('Model', function ($resource, $http, $q, $timeout) {
 			});
 	}
 
+	var generateUniqeId = function() {
+	    var d = new Date().getTime();
+	    if(window.performance && typeof window.performance.now === "function"){
+	        d += performance.now();; //use high-precision timer if available
+	    }
+	    var uid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+	        var r = (d + Math.random()*16)%16 | 0;
+	        d = Math.floor(d/16);
+	        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+	    });
+	    return uid;
+	};
 	
-
+	this.signedIn = function () {
+		// body...
+		if (this.settings.access_token) {
+			return true
+		}else{
+			return false
+		}
+	}
 
 	this.setToken = function (token) {
 		// sets access token.
@@ -40,56 +50,21 @@ spotifyApp.factory('Model', function ($resource, $http, $q, $timeout) {
 	this.getUser = function () {
 		// This should maybe be done by returning a promise 
 		//to the controller wher it can be resolved...
-		return req('/me')
+		var deferred = $q.defer();
+		req('/me').then(function(response) {
+            if (!response || response.error) {
+                deferred.reject('Error occured');
+                console.log(response, "ERROR");
+            } else {
+                deferred.resolve(response);
+                //console.log("SUCCESS")
+                //console.log(response);
+            }
+        });
+        //console.log(deferred.promise);
+        return deferred.promise;
 	}
 
-
-
-	///Aarons model functions
-
-	// this.getProfileInfo = function(){
-	// //hämtar användarens information
-
-	// var req = {
-	//  method: 'GET',
-	//  url: 'https://api.spotify.com/v1/me',
-	//  headers: {
-	//    'Authorization': 'Bearer ' + 'INSERT ACCESS_TOKEN HERE'
-	//  },
-
-	//  data: { test: 'test' }
-	// }
-
- //    var temp = {};
- //    var defer = $q.defer();
- //    $http(req).then(function(data){
- //            temp =data;
- //            defer.resolve(data);
- //    });
- //    return defer.promise;
-	// };
-
-	// this.getTopPlaylists = function(){
-	// //hämtar användarens favortlistor
-
-	// var req = {
-	//  method: 'GET', 
-	//  url: 'https://api.spotify.com/v1/me/playlists?limit=10',
-	//  headers: {
-	//    'Authorization': 'Bearer ' + 'INSERT ACCESS_TOKEN HERE' //behöver kopplas ihop med "getToken" i controller
-	//  },
-	//  data: { test: 'test' }
-	// }
-
- //    var temp = {};
- //    var defer = $q.defer();
- //    $http(req).then(function(data){
- //            temp =data;
- //            defer.resolve(data);
- //            console.log(data)
- //    });
- //    return defer.promise; //ger oss tillgång till objektet
-	// };
 
 	return this;
 });
