29461227c3a24d530483f98df1f103ae2f5db09c
Jonas.J <jan.jonas.johansson@gmail.com>
2016-04-10 15:43:07 +0200

    comitt

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email';
+  var scope = 'user-read-private user-read-email user-top-read user-read-birthdate user-library-read user-follow-read streaming playlist-read-collaborative';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
