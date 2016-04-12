3e579f843a6d338e2dfe81e8b32c0b507b96d323
Jonas.J <jan.jonas.johansson@gmail.com>
2016-04-12 18:58:05 +0200

    styled rated lists, ready to implement fetchin playlists and ratings from database

--- public/js/ctrl/ratedlistsCtrl.js
+++ public/js/ctrl/ratedlistsCtrl.js
@@ -2,4 +2,14 @@ spotifyApp.controller('RatedlistsCtrl', function ($scope) {
 	
 	console.log("RatedlistsCtrl loaded")
 
+	$scope.tableHeads = [
+	"Playlist Avatar",
+	"Name",
+	"Rating",
+	"#Votes"]
+
+	$scope.range = function(n) {
+        return new Array(n);
+    };
+
 });
